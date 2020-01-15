a82695fdf8307004f8d42ba3db30e6ca2e442159
Piseth Ky <ky.piseth@gmail.com>
2020-01-15 09:10:31 -0800

    cleaning up roi_align C++ code

--- FasterRCNN/layers/roi_align.py
+++ FasterRCNN/layers/roi_align.py
@@ -2,51 +2,7 @@ import torch
 import math
 
 from torch import nn
-from torch.autograd import Function
-from torch.autograd.function import once_differentiable
-from torch.nn.modules.utils import _pair
 from ..utils.utils import point_interpolate
-
-
-class _ROIAlign(Function):
-    @staticmethod
-    def forward(ctx, input, roi, output_size, spatial_scale, sampling_ratio, aligned):
-        ctx.save_for_backward(roi)
-        ctx.output_size = _pair(output_size)
-        ctx.spatial_scale = spatial_scale
-        ctx.sampling_ratio = sampling_ratio
-        ctx.input_shape = input.size()
-        ctx.aligned = aligned
-        output = _C.roi_align_forward(
-            input, roi, spatial_scale, output_size[0], output_size[1], sampling_ratio, aligned
-        )
-        return output
-
-    @staticmethod
-    @once_differentiable
-    def backward(ctx, grad_output):
-        rois, = ctx.saved_tensors
-        output_size = ctx.output_size
-        spatial_scale = ctx.spatial_scale
-        sampling_ratio = ctx.sampling_ratio
-        bs, ch, h, w = ctx.input_shape
-        grad_input = _C.roi_align_backward(
-            grad_output,
-            rois,
-            spatial_scale,
-            output_size[0],
-            output_size[1],
-            bs,
-            ch,
-            h,
-            w,
-            sampling_ratio,
-            ctx.aligned,
-        )
-        return grad_input, None, None, None, None, None
-
-
-roi_align = _ROIAlign.apply
 
 
 class ROIAlign(nn.Module):
